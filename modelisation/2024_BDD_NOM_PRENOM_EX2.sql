5e670c06484de627e98660e1f43d3f4a33ce2f88
samybenmaajouz5 <samybenmaajouz5@gmail.com>
2025-05-19 14:00:22 +0000

    2 fichier

---
+++ modelisation/2024_BDD_NOM_PRENOM_EX2.sql
@@ -0,0 +1,12 @@
+SELECT * FROM utilisateur WHERE nom = 'Dupont';
+SELECT * FROM materiel WHERE quantite = 10;
+SELECT * FROM reservation WHERE date_deb = '2025-10-05';
+SELECT * FROM materiel WHERE mobilier = 'table';
+SELECT * FROM materiel WHERE informatique = 'clavier';
+SELECT * FROM utilisateur WHERE prenom = 'Jean';
+SELECT * FROM reservation WHERE date_fin = '2025-5-25';
+
+
+
+
+
